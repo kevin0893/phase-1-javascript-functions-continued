6e701fb32687e700368257e7ff6b15d74b1059b6
+ <cheruiyotke95@gmail.com>
2024-10-06 10:01:07 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+
+
+}const newActivity = "bathe my dog!";
+console.log(saturdayFun(newActivity));
+console.log(saturdayFun());
+function mondayWork(_activity = 'go to the office')
+{
+return`This Monday, I will ${_activity}.`;
+}
+const newMonday =  "work from home";
+console.log(mondayWork(newMonday));
+console.log(mondayWork());
+function wrapAdjective(noun) {
+    return function(adjective) {
+      return `You are ${noun}${adjective}${noun}!`;
+        }}
